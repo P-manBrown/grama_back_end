384d745254a374d6e8ff9828b968c7cd2bb08d2e
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-01-20 03:30:41 +0900

    [finish] user-siginup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
   # devise_token_Authのメソッドを使用可能にする記述
   include DeviseTokenAuth::Concerns::SetUserByToken
+  # # CSRFトークン検証をスキップ
+  # skip_before_action :verify_authenticity_token
+  # # コントローラーのメソッドをヘルパーとしてViewで使用
+  # helper_method :current_user, :user_signed_in?
 end
